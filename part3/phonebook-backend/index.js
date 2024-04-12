0a6e977291e14ee459cb36dbd6442098bee0e333
RiverCX <rivercx@proton.me>
2024-04-13 00:05:53 +0800

    3.7-3.8 phonebook-backend

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+
+morgan.token("body", (req, res) => {
+  const str = JSON.stringify(req.body);
+  return str === "{}" ? "" : str;
+});
+
 app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
